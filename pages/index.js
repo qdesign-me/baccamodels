cb7c073f26bc78b65d31ff9564af8c2d4caccd01
Dzmitry <qdesign.by@gmail.com>
2021-09-15 16:23:58 +0300

    'wip'

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>index page</main>
+      <main>bacca</main>
       <Footer />
     </div>
   );
