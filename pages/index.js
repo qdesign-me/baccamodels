5ae3c40570a994deac6bee7d723ecbd7910af8d8
Dzmitry <qdesign.by@gmail.com>
2021-09-16 11:32:18 +0300

    'wip'

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,33 @@
 import Head from 'next/head';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Bacca</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <main>тут скоро будет красота</main>
-      <Footer />
-    </div>
+
+      <header className="cover fixed flex items-center justify-center h-screen w-full full-bg">
+        <video autoPlay muted loop playsInline>
+          <source src="/video/home.mp4" type="video/mp4" />
+        </video>
+        <main className="bg-white px-40 py-20 relative z-10">
+          <img className="h-20" src="/images/logo.svg" alt="" />
+          <div className="flex justify-around mt-5 text-gray-500 links">
+            <Link href="/">
+              <a className="p-3">Russia</a>
+            </Link>
+            <Link href="/">
+              <a className="p-3">Kazakhstan</a>
+            </Link>
+            <Link href="/">
+              <a className="p-3">Kids</a>
+            </Link>
+          </div>
+        </main>
+      </header>
+    </>
   );
 }
