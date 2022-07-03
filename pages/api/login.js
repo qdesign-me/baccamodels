b55f0fbac044ef3bda040820721207bc47501ec5
Dzmitry <qdesign.by@gmail.com>
2022-07-03 20:01:20 +0300

    'wip'

--- pages/api/login.js
+++ pages/api/login.js
@@ -12,25 +12,22 @@ export default async (req, res) => {
 
     if (!comparePassword(password, found.password)) return res.status(200).json({ status: 'error', message: 'Wrong password or email' });
 
-    const user = { id: found._id.toString(), email: found.email, name: found.name, img: found.img, region: found.region, role: found.role, status: found.status };\
+    const user = { id: found._id.toString(), email: found.email, name: found.name, img: found.img, region: found.region, role: found.role, status: found.status };
 
     var token = jwt.sign(user, process.env.SECRET);
-
-
-
 
     // Set Cookie
     res.setHeader(
       'Set-Cookie',
       cookie.serialize('mytoken', token, {
         httpOnly: false,
-        secure: false,//process.env.NODE_ENV !== 'development',
+        secure: false, //process.env.NODE_ENV !== 'development',
         maxAge: 60 * 60 * 24 * 7, // 1 week
         path: '/',
       })
     );
 
-    res.status(200).json({ user});
+    res.status(200).json({ user });
   } else {
     res.setHeader('Allow', ['POST']);
     res.status(405).json({ message: `Method ${req.method} not allowed` });
